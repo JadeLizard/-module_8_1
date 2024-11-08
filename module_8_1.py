61b70cbcb4ca331d8c9f532f2c941babc69b96a3
JadeLizard <lubovauerbah@gmail.com>
2024-11-08 21:32:08 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+
+def add_everything_up(a, b):
+    try:
+        result = (a + b)
+    except TypeError:
+        result = (str(a)+str(b))
+    print(result)
+
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
