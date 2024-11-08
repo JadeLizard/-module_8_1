df9e2795af81f37a99727a012823bb0fb558bf26
JadeLizard <lubovauerbah@gmail.com>
2024-11-08 22:58:05 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,11 +1,9 @@
-
 def add_everything_up(a, b):
     try:
-        result = (a + b)
+        return (a + b)
     except TypeError:
-        result = (str(a)+str(b))
-    print(result)
+        return (str(a)+str(b))
 
-add_everything_up(123.456, 'строка')
-add_everything_up('яблоко', 4215)
-add_everything_up(123.456, 7)
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
